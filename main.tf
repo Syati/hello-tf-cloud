993e99512a52e541de8378ee9be4be5cc5a8e33c
Syati <mizuki-y@syati.info>
2022-09-07 13:19:10 +0900

    fix

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
-  cloud {
-    organization = "mizuki-y"
+  # cloud {
+  #   organization = "mizuki-y"
 
-    workspaces {
-      name = "hello-tf-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "hello-tf-cloud"
+  #   }
+  # }
   required_providers {
     google = {
       source  = "hashicorp/google"
