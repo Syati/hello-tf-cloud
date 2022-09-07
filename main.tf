3fa8e82a952b68e64ee1f4f038eccbee9e777955
Syati <mizuki-y@syati.info>
2022-09-07 13:32:59 +0900

    remove cloud

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
 terraform {
-   cloud {
-     organization = "mizuki-y"
-
-     workspaces {
-       name = "hello-tf-cloud"
-     }
-   }
-
   required_providers {
     google = {
       source  = "hashicorp/google"
